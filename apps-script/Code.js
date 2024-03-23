948188ab7e627ae80bc3299225daf506033cf664
takatama <takatama@users.noreply.github.com>
2024-03-23 15:45:21 +0900

    Implement doGet function

---
+++ apps-script/Code.js
@@ -0,0 +1,5 @@
+function doGet() {
+  return HtmlService.createHtmlOutputFromFile("index.html")
+    .addMetaTag("viewport", "width=device-width, initial-scale=1")
+    .setTitle("React Router Tutorial");
+}
